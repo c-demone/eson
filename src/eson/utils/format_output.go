a134f325d05b81bddd8e670bee48ec27793491b4
ludah65 <c.demone@hotmail.com>
2023-02-04 05:07:33 +0000

    add string formatter

--- src/eson/utils/format_output.go
+++ src/eson/utils/format_output.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"text/template"
 )
 
 // https://github.com/jedib0t/go-pretty/tree/main/table
-
 func PrettyStuct(data interface{}) (string, error) {
 	val, err := json.MarshalIndent(data, "", "   ")
 	if err != nil {
@@ -14,3 +16,28 @@ func PrettyStuct(data interface{}) (string, error) {
 
 	return string(val), nil
 }
+
+type Fdata map[string]interface{}
+
+func Fstring(format string, data Fdata) (string, error) {
+	t, err := template.New("fstring").Parse(format)
+	if err != nil {
+		return "", fmt.Errorf("error creating template: %v", err)
+	}
+	output := new(bytes.Buffer)
+	if err := t.Execute(output, data); err != nil {
+		return "", fmt.Errorf("error executing template: %v", err)
+	}
+	return output.String(), nil
+}
+
+/*
+func Command(cmd exec.Command) interface{} {
+
+	stdin, err := cmd.StdinPipe()
+
+	if strings.Contains(stdin, "conda info") {
+
+	}
+
+}*/
